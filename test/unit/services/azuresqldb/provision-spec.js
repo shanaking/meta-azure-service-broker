0f7e6b7144da142b24b1430beea26c3b86398fd9
Nicolas Roy <niroy@microsoft.com>
2016-12-23 00:43:25 +0000

    Add TDE implementation and test

--- test/unit/services/azuresqldb/provision-spec.js
+++ test/unit/services/azuresqldb/provision-spec.js
@@ -164,7 +164,8 @@ describe('SqlDb - Provision - Execution', function () {
                     location: 'westus',
                     properties: {
                         administratorLogin: 'azureuser',
-                        administratorLoginPassword: 'c1oudc0w'
+                        administratorLoginPassword: 'c1oudc0w',
+                        transparentDataEncryption: 'Disabled'
                     }
                 },
                 sqldbName: 'fake-db-name',
@@ -205,6 +206,7 @@ describe('SqlDb - Provision - Execution', function () {
             sinon.stub(sqldbOps, 'createFirewallRule').yields(null, { statusCode: HttpStatus.OK });
             sinon.stub(sqldbOps, 'getDatabase').yields(null, { statusCode: HttpStatus.NOT_FOUND });
             sinon.stub(sqldbOps, 'createDatabase').yields(null, {body: {}});
+            sinon.stub(sqldbOps, 'setTransparentDataEncryption').yields(null, {statusCode: HttpStatus.OK});
         });
     
         after(function () {
@@ -216,6 +218,7 @@ describe('SqlDb - Provision - Execution', function () {
             sqldbOps.createFirewallRule.restore();
             sqldbOps.getDatabase.restore();
             sqldbOps.createDatabase.restore();
+            sqldbOps.setTransparentDataEncryption.restore();
         });
     
         it('should not callback error', function (done) {
@@ -239,6 +242,7 @@ describe('SqlDb - Provision - Execution', function () {
             sinon.stub(sqldbOps, 'createFirewallRule').yields(null, { statusCode: HttpStatus.OK });
             sinon.stub(sqldbOps, 'getDatabase').yields(null, { statusCode: HttpStatus.NOT_FOUND });
             sinon.stub(sqldbOps, 'createDatabase').yields(null, {body: {}});
+            sinon.stub(sqldbOps, 'setTransparentDataEncryption').yields(null, {statusCode: HttpStatus.OK});
         });
     
         after(function () {
